4f0a2dac6b55c86a8a0ec703c791365e691366f2
Xiaoyi Liu <lxy878@gmail.com>
2020-04-10 02:30:27 +0000

    Automatically backed up by Learn

--- my_server.rb
+++ my_server.rb
@@ -1,9 +1,1 @@
-class MyServer
-  def call(evn)
-    return [200, {'Content-Type' => 'text/html'}, pretty_response]
-  end
-
-  def pretty_response
-    (Time.to_i % 2).zero? ? [<em>Hello</em>] : [<strong>Hello</strong>]
-  end
-end
+require './my_server'
